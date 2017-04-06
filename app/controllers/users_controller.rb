1d90c426a77cc4d9258e297ca6d77e1b8e3d3abb
Juan Pablo Gonzalez <robin2243@hotmail.com>
2017-04-05 21:58:47 -0700

    refactor user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,44 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
    def index
-   
-  end
+       @users = User.all
+   end
 
   def show
   end
 
   def new
+    @user = User.new
+  end
+  
+ def create
+    @user = User.new(user_params)
+    #use parentesis for readability
+    @user.save ? (redirect_to users_path) : (redirect_to new_user_path)
   end
 
   def edit
   end
 
-  def update   
-      
-    
+  def update 
+    @user.update(user_params) ? (redirect_to users_path) : (redirect_to edit_user_path(user))     
   end
 
   def destroy
-    
+    @user.destroy
+    redirect_to users_path
   end
+  
+  private
+  def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
+
+
 end
